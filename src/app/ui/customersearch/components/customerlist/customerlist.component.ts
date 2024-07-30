e34298f4845130a7fcebc46da70b0a377496fa18
OnurKeles4 <onurkeles2002@hotmail.com>
2024-07-30 11:23:49 +0300

    Added CustomerSearch (WIP)
    Cannot search yet, can access to the database for the customer names.

--- src/app/ui/customersearch/components/customerlist/customerlist.component.ts
+++ src/app/ui/customersearch/components/customerlist/customerlist.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { IxModule } from '@siemens/ix-angular';
+import { AgGridAngular } from 'ag-grid-angular';
 
+import { ColDef } from 'ag-grid-community';
+import { CustomerService } from '../../../../services/common/models/customer.service';
+import { DataService } from '../../../../services/common/dataservice';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-customerlist',
   standalone: true,
-  imports: [],
+  imports: [AgGridAngular, IxModule, CommonModule],
   templateUrl: './customerlist.component.html',
-  styleUrl: './customerlist.component.scss'
+  styleUrl: './customerlist.component.scss',
 })
 export class CustomerlistComponent {
+  isBrowser: boolean;
+  subscription: any;
+  isDataReady: boolean;
 
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private customerService: CustomerService,
+    private dataService: DataService
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+    this.subscription = this.dataService.dataObs.subscribe((data) => {
+      console.log('Data has been set', data);
+    });
+    this.updateList();
+  }
+
+  rowData: any[] = [{ customer_name: 'John Doe' }];
+  defaultColDef: ColDef = {
+    sortable: true,
+    filter: true,
+  };
+  colDefs: ColDef[] = [
+    { field: 'name' }, //customer name
+  ];
+
+  async updateList() {
+    let rowdatatemp = [];
+    this.customerService.read().subscribe((data) => {
+      rowdatatemp = data;
+
+      this.rowData = rowdatatemp;
+      this.isDataReady = true;
+    });
+  }
 }
